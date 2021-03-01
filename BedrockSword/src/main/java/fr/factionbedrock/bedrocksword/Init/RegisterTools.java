d1ac6a0383725329d3716d4939ce5ecd25383033
nicochart <76011128+nicochart@users.noreply.github.com>
2021-03-01 22:02:23 +0100

    First Minecraft mod : BedrockSword
    First version of nicochart's First Minecraft mod

---
+++ BedrockSword/src/main/java/fr/factionbedrock/bedrocksword/Init/RegisterTools.java
@@ -0,0 +1,26 @@
+package fr.factionbedrock.bedrocksword.Init;
+
+import fr.factionbedrock.bedrocksword.Tools.BasisToolMaterial;
+import fr.factionbedrock.bedrocksword.Tools.Sword;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@EventBusSubscriber(modid = "bedrocksword", bus = Bus.MOD)
+public class RegisterTools
+{
+	 public static final Sword bedrockSword = new Sword(BasisToolMaterial.bedrock, new Item.Properties().group(ItemGroup.COMBAT)); //TOOLS
+	 
+	 @SubscribeEvent
+	 public static void register(Register<Item> event)
+	 {
+	        IForgeRegistry<Item> registry = event.getRegistry();
+	        
+	        bedrockSword.setRegistryName("bedrocksword", "bedrock_sword");
+	        registry.registerAll(bedrockSword);
+	 }
+}
