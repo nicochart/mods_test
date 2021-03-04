4a76058147bc1fb013cb67bf5027184eac4017a3
nicochart <76011128+nicochart@users.noreply.github.com>
2021-03-04 15:59:01 +0100

    feat(BedrockStuff) : Working on BedrockOre
    Bedrock Ore harvest level was not working (defined in BedrockOre).
    Moving properties from BedrockOre to RegisterBlocks works.

--- BedrockStuff/src/main/java/fr/factionbedrock/bedrockstuff/Blocks/BedrockOre.java
+++ BedrockStuff/src/main/java/fr/factionbedrock/bedrockstuff/Blocks/BedrockOre.java
@@ -13,9 +13,18 @@ import net.minecraftforge.common.ToolType;
 
 public class BedrockOre extends Block
 {
+	private static int harvestLevel = 4;
+	
 	public BedrockOre()
 	{
-        super(Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(100F,100F).sound(SoundType.STONE));
+		super(Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(90f,100f).sound(SoundType.STONE));
+    }
+	/*Si on appel le super constructeur, les sous paramètres (.harvestLevel, etc) ne sont pas bien pris en compte.
+	  Il est préférable de passer en paramètre les propriétés du bloc au moment de sa création dans RegisterBlocks.*/
+	
+	public BedrockOre(Properties properties)
+    {
+        super(properties);
     }
 
     @Nullable
@@ -23,7 +32,7 @@ public class BedrockOre extends Block
     public ToolType getHarvestTool(BlockState state) {return ToolType.PICKAXE;}
 
     @Override
-    public int getHarvestLevel(BlockState state) {return 4;}
+    public int getHarvestLevel(BlockState state) {return harvestLevel;}
 
     @Override
     public int getExpDrop(BlockState state, IWorldReader world, BlockPos pos, int fortune, int silktouch)
--- BedrockStuff/src/main/java/fr/factionbedrock/bedrockstuff/Register/RegisterBlocks.java
+++ BedrockStuff/src/main/java/fr/factionbedrock/bedrockstuff/Register/RegisterBlocks.java
@@ -3,6 +3,7 @@ package fr.factionbedrock.bedrockstuff.Register;
 import fr.factionbedrock.bedrockstuff.BedrockStuff;
 import fr.factionbedrock.bedrockstuff.Blocks.BedrockOre;
 import fr.factionbedrock.bedrockstuff.Item.ItemFromBlock;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -12,11 +13,24 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraftforge.common.ToolType;
+import net.minecraft.block.SoundType;
+
 @EventBusSubscriber(modid = BedrockStuff.MODID, bus = Bus.MOD)
 public class RegisterBlocks
 {
-	public static final Block bedrockOre = new BedrockOre();
-	
+	public static final Block bedrockOre = new BedrockOre
+			(
+			    AbstractBlock.Properties.create(Material.ROCK, MaterialColor.STONE)
+               .hardnessAndResistance(90f, 100f)
+               .harvestTool(ToolType.PICKAXE)
+               .harvestLevel(4)
+               .sound(SoundType.STONE)
+               .setRequiresTool()
+            );
+      
 	@SubscribeEvent
     public static void register(Register<Block> event)
 	{
