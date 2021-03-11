1c0fc8b3e906f89e12163d577532ea12329a5e0d
nicochart <76011128+nicochart@users.noreply.github.com>
2021-03-11 14:44:53 +0100

    feat(NewDim) : Copying a Minecraft class and trying again
    - Copy of NoiseChunkGenerator
    (because a class can't extend NoiseChunkGenerator..)
    - Editing NewDimChunkGenerator to make it "work" (I hope)
    There is no longer errors

--- NewDimension/src/main/java/fr/factionbedrock/newdim/World/NewDimChunkGenerator.java
+++ NewDimension/src/main/java/fr/factionbedrock/newdim/World/NewDimChunkGenerator.java
@@ -22,67 +22,31 @@ import net.minecraft.world.gen.Heightmap.Type;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class NewDimChunkGenerator extends ChunkGenerator
+public class NewDimChunkGenerator extends NewNoiseChunkGenerator
 {
-	public static final Codec<NewDimChunkGenerator> CODEC = RecordCodecBuilder.create(
-            (instance) -> instance.group(
-                    BiomeProvider.CODEC.fieldOf("biome_source")
-                            .forGetter((chunkGenerator) -> chunkGenerator.biomeSource),
-                    Codec.LONG.fieldOf("seed")
-                            .orElseGet(NewDimSeed::getSeed)
-                            .forGetter((chunkGenerator) -> chunkGenerator.seed),
-                    DimensionSettings.CODEC.fieldOf("settings")
-                            .forGetter((chunkGenerator) -> chunkGenerator.settings))
-                    .apply(instance, instance.stable(NewDimChunkGenerator::new)));
+	public static final Codec<NewNoiseChunkGenerator> CODEC = RecordCodecBuilder.create((p_236091_0_) -> {
+	      return p_236091_0_.group(BiomeProvider.CODEC.fieldOf("biome_source").forGetter((p_236096_0_) -> {
+	         return p_236096_0_.getBiomeProvider();
+	      }), Codec.LONG.fieldOf("seed").stable().forGetter((p_236093_0_) -> {
+	         return p_236093_0_.field_236084_w_;
+	      }), DimensionSettings.field_236098_b_.fieldOf("settings").forGetter((p_236090_0_) -> {
+	         return p_236090_0_.field_236080_h_;
+	      })).apply(p_236091_0_, p_236091_0_.stable(NewNoiseChunkGenerator::new));
+	   });
 	
-	protected final BiomeProvider biomeSource;
-	
-    /*public NewDimChunkGenerator(BiomeProvider biomeProvider, long seed, Supplier<DimensionSettings> dimensionSettingsSupplier)
-    {
-        super(biomeProvider, (DimensionStructuresSettings) dimensionSettingsSupplier);
-        this.seed=seed;
-    }*/
-    
-    public NewDimChunkGenerator(BiomeProvider BiomeProvider1, BiomeProvider BiomeProvider2, DimensionStructuresSettings settings, long seed)
-    {
-		super(BiomeProvider1,BiomeProvider2,settings,seed);
-		this.biomeSource = null;
+    public NewDimChunkGenerator(BiomeProvider biomeProvider, long seed, Supplier<DimensionSettings> dimensionSettingsSupplier) {
+        super(biomeProvider, seed, dimensionSettingsSupplier);
     }
-    
-    //func_230347_a_() == codec()
-	@Override
-	protected Codec<? extends ChunkGenerator> func_230347_a_() {
-		// TODO Auto-generated method stub
-		return CODEC;
-	}
-	//func_230349_a_ == withSeed
-	@OnlyIn(Dist.CLIENT)
-	@Override
-	public ChunkGenerator func_230349_a_(long seed) {
-		return null;//new NewDimChunkGenerator(this.biomeSource.func_230349_a_(seed), seed, this.settings);
-	}
 
-	@Override
-	public void generateSurface(WorldGenRegion p_225551_1_, IChunk p_225551_2_) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    protected Codec<? extends ChunkGenerator> func_230347_a_() {
+        return CODEC;
+    }
 
-	@Override
-	public void func_230352_b_(IWorld p_230352_1_, StructureManager p_230352_2_, IChunk p_230352_3_) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getHeight(int x, int z, Type heightmapType) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public IBlockReader func_230348_a_(int p_230348_1_, int p_230348_2_) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    //func_230349_a_ = withSeed
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public ChunkGenerator func_230349_a_(long p_230349_1_) {
+        return new NewNoiseChunkGenerator(this.biomeProvider.getBiomeProvider(p_230349_1_), p_230349_1_, this.field_236080_h_);
+     }
 }
