212c9e8a5867b8a2a68a10837870c3da18ac1ad8
nicochart <76011128+nicochart@users.noreply.github.com>
2021-03-17 19:42:46 +0100

    feat(NewDim) : White Aercloud is now translucent

---
+++ NewDimension/src/main/java/fr/factionbedrock/newdim/Client/NewDimRendering.java
@@ -0,0 +1,28 @@
+package fr.factionbedrock.newdim.Client;
+
+import com.google.common.base.Supplier;
+
+import fr.factionbedrock.newdim.NewDimension;
+import fr.factionbedrock.newdim.Setup.Registration;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = NewDimension.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@OnlyIn(Dist.CLIENT)
+public class NewDimRendering
+{
+	public static void registerBlockRenderLayers()
+	{
+		RenderType translucent = RenderType.getTranslucent();
+        render(() -> Registration.NEWDIM_WHITE_AERCLOUD.get(), translucent);
+	}
+	
+	private static void render(Supplier<? extends Block> block, RenderType render)
+	{
+        RenderTypeLookup.setRenderLayer(block.get(), render);
+    }
+}
