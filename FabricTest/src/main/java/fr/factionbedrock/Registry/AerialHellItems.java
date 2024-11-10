5bc26bdbb32bb1a9f2d603cddc7ba7e4b35b7506
Nicolas Hochart <nicolas@freddom.fr>
2024-11-10 20:06:43 +0100

    feat(FabricTest) : block assets & one more item

--- FabricTest/src/main/java/fr/factionbedrock/Registry/AerialHellItems.java
+++ FabricTest/src/main/java/fr/factionbedrock/Registry/AerialHellItems.java
@@ -8,7 +8,7 @@ import net.minecraft.registry.Registry;
 
 public class AerialHellItems
 {
-    //public static final Item AERIAL_TREE_PLANKS = register("aerial_tree_planks", new Item(new Item.Settings()));
+    public static final Item SKY_STICK = register("sky_stick", new Item(new Item.Settings()));
     public static final Item AERIAL_TREE_PLANKS = register("aerial_tree_planks", new BlockItem(AerialHellBlocks.AERIAL_TREE_PLANKS, new Item.Settings()));
 
     public static <T extends Item> T register(String name, T item) {return Registry.register(Registries.ITEM, AerialHell.id(name), item);}
