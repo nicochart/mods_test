af477d7ea2546931e6dc2a19f05197da8773c81f
Nicolas Hochart <nicolas@freddom.fr>
2024-11-13 22:47:25 +0100

    feat(FabricTest) : Spawn egg Item

--- FabricTest/src/main/java/fr/factionbedrock/Registry/AerialHellItems.java
+++ FabricTest/src/main/java/fr/factionbedrock/Registry/AerialHellItems.java
@@ -3,6 +3,7 @@ package fr.factionbedrock.Registry;
 import fr.factionbedrock.AerialHell;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
@@ -10,6 +11,7 @@ public class AerialHellItems
 {
     public static final Item SKY_STICK = register("sky_stick", new Item(new Item.Settings()));
     public static final Item AERIAL_TREE_PLANKS = register("aerial_tree_planks", new BlockItem(AerialHellBlocks.AERIAL_TREE_PLANKS, new Item.Settings()));
+    public static final Item EVIL_COW_SPAWN_EGG = register("evil_cow_spawn_egg", new SpawnEggItem(AerialHellEntityTypes.EVIL_COW, -1, -1, new Item.Settings()));
 
     public static <T extends Item> T register(String name, T item) {return Registry.register(Registries.ITEM, AerialHell.id(name), item);}
 
