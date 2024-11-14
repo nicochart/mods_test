0fc7bccb03944483399f95a3c14af0c1273b8362
Nicolas Hochart <nicolas@freddom.fr>
2024-11-14 17:28:42 +0100

    feat(FabricTest) : Mixin to detect specific block placement

---
+++ FabricTest/src/main/java/fr/factionbedrock/Mixin/NeighborNotifyMixin.java
@@ -0,0 +1,45 @@
+package fr.factionbedrock.Mixin;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(Block.class)
+public class NeighborNotifyMixin
+{
+	//net.minecraft.block.Block.onPlaced
+	@Inject(method = "onPlaced", at = @At("HEAD"), cancellable = true)
+	private void onBlockPlacedBy(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack, CallbackInfo callbackInfo)
+	{
+		if (world.isClient) {return;}
+
+		if (world.getDimensionEntry().matchesId(DimensionTypes.THE_END.getValue()))
+		{
+			if (state.getBlock() == Blocks.TORCH || state.getBlock() == Blocks.WALL_TORCH)
+			{
+				if (placer.getServer() != null)
+				{
+					List<ServerPlayerEntity> players = placer.getServer().getPlayerManager().getPlayerList();
+					players.forEach(player -> player.sendMessage(Text.literal(player.getName().getString() + " tried to place a torch in the END at x="+pos.getX()+", y="+pos.getY()+", z="+pos.getZ()+" ...!")));
+				}
+
+				world.breakBlock(pos, true);
+				callbackInfo.cancel();
+			}
+		}
+	}
+}
