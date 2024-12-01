935829f1b9dc3aee83657ecd82a3ef9ac916e52b
Nicolas Hochart <nicolas@freddom.fr>
2024-12-01 23:59:21 +0100

    feat(FabricTest) : Rendering (inventory) item names with custom colors

---
+++ FabricTest/src/main/java/fr/factionbedrock/Mixin/CustomHeldItemNameRenderMixin.java
@@ -0,0 +1,53 @@
+package fr.factionbedrock.Mixin;
+
+import fr.factionbedrock.Registry.AerialHellItems;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.ColorHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class CustomHeldItemNameRenderMixin
+{
+    @Inject(method = "renderHeldItemTooltip", at = @At("HEAD"), cancellable = true)
+    private void modifyItemName(DrawContext context, CallbackInfo ci)
+    {
+        InGameHud hud = (InGameHud) (Object) this;
+        ItemStack stack = hud.currentStack;
+
+        if (stack.getItem() == AerialHellItems.EVIL_COW_SPAWN_EGG)
+        {
+            ci.cancel();
+
+            //edited copy of InGameHud.renderHeldItemTooltip
+            hud.client.getProfiler().push("selectedItemName");
+            if (hud.heldItemTooltipFade > 0 && !stack.isEmpty())
+            {
+                //What is done in vanilla code
+                //MutableText mutableText = Text.empty().append(hud.currentStack.getName()).formatted(hud.currentStack.getRarity().getFormatting());
+                //if (hud.currentStack.contains(DataComponentTypes.CUSTOM_NAME)) {mutableText.formatted(Formatting.ITALIC);}
+
+                MutableText mutableText = Text.empty().append(stack.getName()).formatted(Formatting.DARK_RED).formatted(Formatting.BOLD);
+
+                int i = hud.getTextRenderer().getWidth(mutableText);
+                int j = (context.getScaledWindowWidth() - i) / 2;
+                int k = context.getScaledWindowHeight() - 59;
+                if (!hud.client.interactionManager.hasStatusBars()) {k += 14;}
+
+                int opacity = (int)((float)hud.heldItemTooltipFade * 256.0F / 10.0F);
+                if (opacity > 255) {opacity = 255;}
+
+                if (opacity > 0) {context.drawTextWithBackground(hud.getTextRenderer(), mutableText, j, k, i, ColorHelper.Argb.withAlpha(opacity, -1));}
+            }
+
+            hud.client.getProfiler().pop();
+        }
+    }
+}
--- FabricTest/src/main/java/fr/factionbedrock/Mixin/CustomItemNameRenderMixin.java
+++ FabricTest/src/main/java/fr/factionbedrock/Mixin/CustomItemNameRenderMixin.java
@@ -1,53 +1,98 @@
 package fr.factionbedrock.Mixin;
 
+import com.google.common.collect.Lists;
 import fr.factionbedrock.Registry.AerialHellItems;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.item.ItemStack;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.MapIdComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.Registries;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.ColorHelper;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(InGameHud.class)
+import java.util.List;
+import java.util.function.Consumer;
+
+@Mixin(ItemStack.class)
 public class CustomItemNameRenderMixin
 {
-    @Inject(method = "renderHeldItemTooltip", at = @At("HEAD"), cancellable = true)
-    private void modifyItemName(DrawContext context, CallbackInfo ci)
+    private static final Text DISABLED_TEXT = Text.translatable("item.disabled").formatted(Formatting.RED);
+
+    @Inject(method = "getTooltip", at = @At("HEAD"), cancellable = true)
+    private void modifyItemName(Item.TooltipContext context, @Nullable PlayerEntity player, TooltipType type, CallbackInfoReturnable<List<Text>> cir)
     {
-        InGameHud hud = (InGameHud) (Object) this;
-        ItemStack stack = hud.currentStack;
-
-        if (stack.getItem() == AerialHellItems.EVIL_COW_SPAWN_EGG)
+        //edited copy of ItemStack.getTooltip
+        ItemStack itemstack = (ItemStack) (Object) this;
+        if (!type.isCreative() && itemstack.contains(DataComponentTypes.HIDE_TOOLTIP)) {cir.setReturnValue(List.of()); return;}
+        else
         {
-            ci.cancel();
+            List<Text> list = Lists.<Text>newArrayList();
+            //What is done in vanilla code
+            //MutableText mutableText = Text.empty().append(itemstack.getName()).formatted(itemstack.getRarity().getFormatting());
+            //if (itemstack.contains(DataComponentTypes.CUSTOM_NAME)) {mutableText.formatted(Formatting.ITALIC);}
 
-            //edited copy of InGameHud.renderHeldItemTooltip
-            hud.client.getProfiler().push("selectedItemName");
-            if (hud.heldItemTooltipFade > 0 && !stack.isEmpty())
+            MutableText mutableText = itemstack.getItem() == AerialHellItems.EVIL_COW_SPAWN_EGG ? Text.empty().append(itemstack.getName()).formatted(Formatting.DARK_RED).formatted(Formatting.BOLD) : Text.empty().append(itemstack.getName()).formatted(Formatting.DARK_GREEN);
+
+            list.add(mutableText);
+            if (!type.isAdvanced() && !itemstack.contains(DataComponentTypes.CUSTOM_NAME) && itemstack.isOf(Items.FILLED_MAP))
             {
-                //What is done in vanilla code
-                //MutableText mutableText = Text.empty().append(hud.currentStack.getName()).formatted(hud.currentStack.getRarity().getFormatting());
-                //if (hud.currentStack.contains(DataComponentTypes.CUSTOM_NAME)) {mutableText.formatted(Formatting.ITALIC);}
-
-                MutableText mutableText = Text.empty().append(stack.getName()).formatted(Formatting.DARK_RED).formatted(Formatting.BOLD);
-
-                int i = hud.getTextRenderer().getWidth(mutableText);
-                int j = (context.getScaledWindowWidth() - i) / 2;
-                int k = context.getScaledWindowHeight() - 59;
-                if (!hud.client.interactionManager.hasStatusBars()) {k += 14;}
-
-                int opacity = (int)((float)hud.heldItemTooltipFade * 256.0F / 10.0F);
-                if (opacity > 255) {opacity = 255;}
-
-                if (opacity > 0) {context.drawTextWithBackground(hud.getTextRenderer(), mutableText, j, k, i, ColorHelper.Argb.withAlpha(opacity, -1));}
+                MapIdComponent mapIdComponent = itemstack.get(DataComponentTypes.MAP_ID);
+                if (mapIdComponent != null) {list.add(FilledMapItem.getIdText(mapIdComponent));}
             }
 
-            hud.client.getProfiler().pop();
+            Consumer<Text> consumer = list::add;
+            if (!itemstack.contains(DataComponentTypes.HIDE_ADDITIONAL_TOOLTIP)) {itemstack.getItem().appendTooltip(itemstack, context, list, type);}
+
+            itemstack.appendTooltip(DataComponentTypes.JUKEBOX_PLAYABLE, context, consumer, type);
+            itemstack.appendTooltip(DataComponentTypes.TRIM, context, consumer, type);
+            itemstack.appendTooltip(DataComponentTypes.STORED_ENCHANTMENTS, context, consumer, type);
+            itemstack.appendTooltip(DataComponentTypes.ENCHANTMENTS, context, consumer, type);
+            itemstack.appendTooltip(DataComponentTypes.DYED_COLOR, context, consumer, type);
+            itemstack.appendTooltip(DataComponentTypes.LORE, context, consumer, type);
+            itemstack.appendAttributeModifiersTooltip(consumer, player);
+            itemstack.appendTooltip(DataComponentTypes.UNBREAKABLE, context, consumer, type);
+            BlockPredicatesChecker blockPredicatesChecker = itemstack.get(DataComponentTypes.CAN_BREAK);
+            if (blockPredicatesChecker != null && blockPredicatesChecker.showInTooltip())
+            {
+                consumer.accept(ScreenTexts.EMPTY);
+                consumer.accept(BlockPredicatesChecker.CAN_BREAK_TEXT);
+                blockPredicatesChecker.addTooltips(consumer);
+            }
+
+            BlockPredicatesChecker blockPredicatesChecker2 = itemstack.get(DataComponentTypes.CAN_PLACE_ON);
+            if (blockPredicatesChecker2 != null && blockPredicatesChecker2.showInTooltip())
+            {
+                consumer.accept(ScreenTexts.EMPTY);
+                consumer.accept(BlockPredicatesChecker.CAN_PLACE_TEXT);
+                blockPredicatesChecker2.addTooltips(consumer);
+            }
+
+            if (type.isAdvanced())
+            {
+                if (itemstack.isDamaged())
+                {
+                    list.add(Text.translatable("item.durability", itemstack.getMaxDamage() - itemstack.getDamage(), itemstack.getMaxDamage()));
+                }
+
+                list.add(Text.literal(Registries.ITEM.getId(itemstack.getItem()).toString()).formatted(Formatting.DARK_GRAY));
+                int i = itemstack.components.size();
+                if (i > 0) {list.add(Text.translatable("item.components", i).formatted(Formatting.DARK_GRAY));}
+            }
+
+            if (player != null && !itemstack.getItem().isEnabled(player.getWorld().getEnabledFeatures())) {list.add(DISABLED_TEXT);}
+            cir.setReturnValue(list);
         }
     }
 }
