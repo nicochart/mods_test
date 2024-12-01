fe2e3e8a012ff1f32c821ceaa9a1e153fdc8800f
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2024-12-01 03:35:19 +0100

    feat(FabricTest) : Rendering Held Item Tooltip differently

--- FabricTest/src/main/java/fr/factionbedrock/Mixin/CustomItemNameRenderMixin.java
+++ FabricTest/src/main/java/fr/factionbedrock/Mixin/CustomItemNameRenderMixin.java
@@ -1,8 +1,12 @@
 package fr.factionbedrock.Mixin;
 
+import fr.factionbedrock.Registry.AerialHellItems;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,19 +16,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class CustomItemNameRenderMixin
 {
-    @Inject(method = "renderHeldItemTooltip", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "renderHeldItemTooltip", at = @At("HEAD"), cancellable = true)
     private void modifyItemName(DrawContext context, CallbackInfo ci)
     {
-        //edited copy of renderHeldItemTooltip
         InGameHud hud = (InGameHud) (Object) this;
-        Text text = Text.empty().append("Text above health bar");
+        ItemStack stack = hud.currentStack;
 
-        int i = hud.getTextRenderer().getWidth(text);
-        int j = (context.getScaledWindowWidth() - i) / 2;
-        int k = context.getScaledWindowHeight() - 59;
+        if (stack.getItem() == AerialHellItems.EVIL_COW_SPAWN_EGG)
+        {
+            ci.cancel();
 
-        int opacity = 255;
+            //edited copy of InGameHud.renderHeldItemTooltip
+            hud.client.getProfiler().push("selectedItemName");
+            if (hud.heldItemTooltipFade > 0 && !stack.isEmpty())
+            {
+                //What is done in vanilla code
+                //MutableText mutableText = Text.empty().append(hud.currentStack.getName()).formatted(hud.currentStack.getRarity().getFormatting());
+                //if (hud.currentStack.contains(DataComponentTypes.CUSTOM_NAME)) {mutableText.formatted(Formatting.ITALIC);}
 
-        if (opacity > 0) {context.drawTextWithBackground(hud.getTextRenderer(), text, j, k, i, ColorHelper.Argb.withAlpha(opacity, -1));}
+                MutableText mutableText = Text.empty().append(stack.getName()).formatted(Formatting.DARK_RED).formatted(Formatting.BOLD);
+
+                int i = hud.getTextRenderer().getWidth(mutableText);
+                int j = (context.getScaledWindowWidth() - i) / 2;
+                int k = context.getScaledWindowHeight() - 59;
+                if (!hud.client.interactionManager.hasStatusBars()) {k += 14;}
+
+                int opacity = (int)((float)hud.heldItemTooltipFade * 256.0F / 10.0F);
+                if (opacity > 255) {opacity = 255;}
+
+                if (opacity > 0) {context.drawTextWithBackground(hud.getTextRenderer(), mutableText, j, k, i, ColorHelper.Argb.withAlpha(opacity, -1));}
+            }
+
+            hud.client.getProfiler().pop();
+        }
     }
 }
