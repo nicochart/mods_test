ee7d61ea4f0b48c2997c616c219df1dd9b2e4551
Nicolas Hochart <nicolas@freddom.fr>
2024-12-08 13:20:36 +0100

    feat(FabricTest) : new method to register Entities

--- FabricTest/src/main/java/fr/factionbedrock/Registry/AerialHellEntityTypes.java
+++ FabricTest/src/main/java/fr/factionbedrock/Registry/AerialHellEntityTypes.java
@@ -11,7 +11,12 @@ import net.minecraft.registry.Registry;
 
 public class AerialHellEntityTypes
 {
-    public static final EntityType<EvilCowEntity> EVIL_COW = register("evil_cow", EntityType.Builder.<EvilCowEntity>create(EvilCowEntity::new, SpawnGroup.MONSTER).dimensions(0.99F, 1.4F).build("evil_cow"));
+    public static final EntityType<EvilCowEntity> EVIL_COW = register("evil_cow", EvilCowEntity::new, 0.99F, 1.4F, SpawnGroup.MONSTER);
+
+    public static <E extends Entity> EntityType<E> register(String id, EntityType.EntityFactory<E> factory, float width, float height, SpawnGroup group)
+    {
+        return register(id, EntityType.Builder.create(factory, group).dimensions(width, height).build(id));
+    }
 
     public static <T extends EntityType<? extends Entity>> T register(String name, T entityType) {return Registry.register(Registries.ENTITY_TYPE, AerialHell.id(name), entityType);}
 
