dddd233ce77c6229aeb8afcaf974a9ba20b3e725
Nicolas Hochart <nicolas@freddom.fr>
2024-12-16 19:21:08 +0100

    feat(FabricTest) : Accessing glowstone baked model differently

--- FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
+++ FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
@@ -2,10 +2,10 @@ package fr.factionbedrock.Client.BlockBakedModels;
 
 import fr.factionbedrock.Registry.AerialHellBlocks;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.render.block.BlockModels;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.ModelIdentifier;
-import net.minecraft.util.Identifier;
 
 public class AerialHellShiftingBakedModels
 {
@@ -14,8 +14,7 @@ public class AerialHellShiftingBakedModels
         ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((original, context) ->
         {
             ModelIdentifier initialIdentifier = BlockModels.getModelId(AerialHellBlocks.AERIAL_TREE_PLANKS.getDefaultState());
-            ModelIdentifier glowstoneModelIdentifier = ModelIdentifier.ofInventoryVariant(Identifier.ofVanilla("glowstone"));
-            BakedModel editedModel = context.loader().getBakedModelMap().get(glowstoneModelIdentifier);
+            BakedModel editedModel = context.loader().getBakedModelMap().get(BlockModels.getModelId(Blocks.GLOWSTONE.getDefaultState()));
             context.loader().getBakedModelMap().put(initialIdentifier, editedModel);
             return original;
         }));
