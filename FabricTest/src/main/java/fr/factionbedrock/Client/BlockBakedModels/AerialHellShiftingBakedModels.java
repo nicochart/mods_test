5340f4eaec70466e4f306cf38a4285251fd362cd
Nicolas Hochart <nicolas@freddom.fr>
2024-12-16 20:40:10 +0100

    feat(FabricTest) : Shifting block baked model

--- FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
+++ FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
@@ -1,11 +1,15 @@
 package fr.factionbedrock.Client.BlockBakedModels;
 
 import fr.factionbedrock.Registry.AerialHellBlocks;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.block.BlockModels;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class AerialHellShiftingBakedModels
 {
@@ -14,9 +18,14 @@ public class AerialHellShiftingBakedModels
         ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((original, context) ->
         {
             ModelIdentifier initialIdentifier = BlockModels.getModelId(AerialHellBlocks.AERIAL_TREE_PLANKS.getDefaultState());
-            BakedModel editedModel = context.loader().getBakedModelMap().get(BlockModels.getModelId(Blocks.GLOWSTONE.getDefaultState()));
+            ModelIdentifier shiftedIdentifier = BlockModels.getModelId(Blocks.GLOWSTONE.getDefaultState());
+            BakedModel initialModel = context.loader().getBakedModelMap().get(initialIdentifier);
+            BakedModel shiftedModel = context.loader().getBakedModelMap().get(shiftedIdentifier);
+            BakedModel editedModel = new ShiftingBlockBakedModel(initialModel, shiftedModel, (forceShifted) -> doesCurrentPlayerHaveNightVision() || forceShifted);
             context.loader().getBakedModelMap().put(initialIdentifier, editedModel);
             return original;
         }));
     }
+
+    public static boolean doesCurrentPlayerHaveNightVision() {return FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT && MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().player.hasStatusEffect(StatusEffects.NIGHT_VISION);}
 }
---
+++ FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/ShiftingBlockBakedModel.java
@@ -0,0 +1,43 @@
+package fr.factionbedrock.Client.BlockBakedModels;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.BakedQuad;
+import net.minecraft.client.render.model.json.ModelOverrideList;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class ShiftingBlockBakedModel implements BakedModel
+{
+    private final BakedModel defaultModel;
+    private final BakedModel shiftedModel;
+    private final Function<Boolean, Boolean> shouldDisplayShiftedModel;
+
+    public ShiftingBlockBakedModel(BakedModel defaultModel, BakedModel shiftedModel, Function<Boolean, Boolean> shouldDisplayShiftedModel)
+    {
+        this.defaultModel = defaultModel;
+        this.shiftedModel = shiftedModel;
+        this.shouldDisplayShiftedModel = shouldDisplayShiftedModel;
+    }
+
+    @Override public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand) {return getModel().getQuads(state, side, rand);}
+    @Override public boolean useAmbientOcclusion() {return getModel().useAmbientOcclusion();}
+    @Override public boolean hasDepth() {return getModel().hasDepth();}
+    @Override public boolean isSideLit() {return getModel().isSideLit();}
+    @Override public boolean isBuiltin() {return getModel().isBuiltin();}
+    @Override public Sprite getParticleSprite() {return getModel().getParticleSprite();}
+    @Override public ModelTransformation getTransformation() {return getModel().getTransformation();}
+    @Override public ModelOverrideList getOverrides() {return getModel().getOverrides();}
+
+    private BakedModel getModel()
+    {
+        return shouldDisplayShiftedModel(false) ? shiftedModel : defaultModel;
+    }
+
+    protected boolean shouldDisplayShiftedModel(boolean forceDefault) {return this.shouldDisplayShiftedModel.apply(forceDefault);}
+}
