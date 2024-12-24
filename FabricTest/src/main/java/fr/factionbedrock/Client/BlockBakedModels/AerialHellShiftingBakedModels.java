cabbe19e914eae410cb24ce6dcb687d75296dcac
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2024-12-24 01:59:28 +0100

    feat(FabricTest) : trying to find a better way to shift Bake Model

--- FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
+++ FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
@@ -3,13 +3,16 @@ package fr.factionbedrock.Client.BlockBakedModels;
 import fr.factionbedrock.Registry.AerialHellBlocks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.block.BlockModels;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.registry.Registries;
 
 public class AerialHellShiftingBakedModels
 {
@@ -17,14 +20,30 @@ public class AerialHellShiftingBakedModels
     {
         ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((original, context) ->
         {
-            ModelIdentifier initialIdentifier = BlockModels.getModelId(AerialHellBlocks.AERIAL_TREE_PLANKS.getDefaultState());
-            ModelIdentifier shiftedIdentifier = BlockModels.getModelId(Blocks.GLOWSTONE.getDefaultState());
-            BakedModel initialModel = context.loader().getBakedModelMap().get(initialIdentifier);
-            BakedModel shiftedModel = context.loader().getBakedModelMap().get(shiftedIdentifier);
-            BakedModel editedModel = new ShiftingBlockBakedModel(initialModel, shiftedModel, (forceShifted) -> doesCurrentPlayerHaveNightVision() || forceShifted);
-            context.loader().getBakedModelMap().put(initialIdentifier, editedModel);
+            if (validateContext(context, AerialHellBlocks.AERIAL_TREE_PLANKS))
+            {
+                ModelIdentifier shiftedIdentifier = BlockModels.getModelId(Blocks.GLOWSTONE.getDefaultState());
+                BakedModel initialModel = original;
+                BakedModel shiftedModel = context.loader().getBakedModelMap().get(shiftedIdentifier);
+                //context.loader().getBakedModelMap().put(initialIdentifier, editedModel);
+                if (initialModel != null && shiftedModel != null)
+                {
+                    BakedModel editedModel = new ShiftingBlockBakedModel(initialModel, shiftedModel, (forceShifted) -> doesCurrentPlayerHaveNightVision() || forceShifted);
+                    return editedModel;
+                }
+                else
+                {
+                    //System.out.println("VALIDATED CONTEXT BUT NULL FOUND : initialModel = "+initialModel+", shiftedModel = "+shiftedModel);
+                }
+            }
             return original;
         }));
+    }
+
+    private static boolean validateContext(ModelModifier.AfterBake.Context context, Block block)
+    {
+        if (context.topLevelId() == null) {return false;}
+        return context.topLevelId().id().equals(Registries.BLOCK.getId(block));
     }
 
     public static boolean doesCurrentPlayerHaveNightVision() {return FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT && MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().player.hasStatusEffect(StatusEffects.NIGHT_VISION);}
