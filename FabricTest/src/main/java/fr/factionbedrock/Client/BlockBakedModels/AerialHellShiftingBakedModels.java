542aac01c6e894c45a37399210e2b2bea58949db
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2024-12-24 13:02:41 +0100

    feat(FabricTest) : loading model variants from bakedModelMap

--- FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
+++ FabricTest/src/main/java/fr/factionbedrock/Client/BlockBakedModels/AerialHellShiftingBakedModels.java
@@ -10,33 +10,41 @@ import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.block.BlockModels;
 import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 
 public class AerialHellShiftingBakedModels
 {
     public static void registerShiftingBakedModels()
     {
-        ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((original, context) ->
+        ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((initialModel, context) ->
         {
             if (validateContext(context, AerialHellBlocks.AERIAL_TREE_PLANKS))
             {
                 ModelIdentifier shiftedIdentifier = BlockModels.getModelId(Blocks.GLOWSTONE.getDefaultState());
-                BakedModel initialModel = original;
                 BakedModel shiftedModel = context.loader().getBakedModelMap().get(shiftedIdentifier);
-                //context.loader().getBakedModelMap().put(initialIdentifier, editedModel);
                 if (initialModel != null && shiftedModel != null)
                 {
-                    BakedModel editedModel = new ShiftingBlockBakedModel(initialModel, shiftedModel, (forceShifted) -> doesCurrentPlayerHaveNightVision() || forceShifted);
-                    return editedModel;
-                }
-                else
-                {
-                    //System.out.println("VALIDATED CONTEXT BUT NULL FOUND : initialModel = "+initialModel+", shiftedModel = "+shiftedModel);
+                    return new ShiftingBlockBakedModel(initialModel, shiftedModel, (forceShifted) -> doesCurrentPlayerHaveNightVision() || forceShifted);
                 }
             }
-            return original;
+            else if (validateContext(context, Blocks.CHERRY_STAIRS))
+            {
+                System.out.println("variant = "+context.topLevelId().getVariant());
+                Identifier shiftedId = Registries.BLOCK.getId(Blocks.BLACKSTONE_STAIRS);
+                ModelIdentifier shiftedIdentifier = new ModelIdentifier(shiftedId, context.topLevelId().getVariant());
+
+                BakedModel shiftedModel = context.loader().getBakedModelMap().get(shiftedIdentifier); //sometimes null : modelBake is not finished, the BakedModelMap is not complete.
+                if (initialModel != null && shiftedModel != null)
+                {
+                    return new ShiftingBlockBakedModel(initialModel, shiftedModel, (forceShifted) -> doesCurrentPlayerHaveNightVision() || forceShifted);
+                }
+                else {System.out.println("NULL FOUND : "+(initialModel == null ? "initialModel " : "")+(shiftedModel == null ? "shiftedModel " : "")+", shiftedIdentifier = "+shiftedIdentifier+", shiftedId = "+shiftedId);}
+            }
+            return initialModel;
         }));
     }
 
