2dc8374758083b354aa87565089d2d10f2e7dc2a
Nicolas Hochart <nicolas@freddom.fr>
2025-01-24 22:01:41 +0100

    feat(NeoForge_1_21_4_Tests) : Added a texture to example_block block & item

--- NeoForge_1_21_4_Tests/src/main/java/com/example/examplemod/block/NeighborChangedTestBlock.java
+++ NeoForge_1_21_4_Tests/src/main/java/com/example/examplemod/block/NeighborChangedTestBlock.java
@@ -1,12 +1,9 @@
 package com.example.examplemod.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.players.PlayerList;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -25,6 +22,7 @@ public class NeighborChangedTestBlock extends Block
 
         if (level instanceof ServerLevel serverLevel)
         {
+            //looks like orientation is always null
             String orientationString = "orientation = "+ (orientation == null ? "null" : orientation.toString() + (orientation.getFront() == null ? "" : ", front = "+orientation.getFront()));
             List<ServerPlayer> playerList = serverLevel.getServer().getPlayerList().getPlayers();
             playerList.forEach(player -> player.sendSystemMessage(Component.literal("neighborChanged at "+pos.getX()+", y="+pos.getY()+", z="+pos.getZ()+", "+orientationString)));
--- NeoForge_1_21_4_Tests/src/main/java/com/example/examplemod/registry/ExampleItems.java
+++ NeoForge_1_21_4_Tests/src/main/java/com/example/examplemod/registry/ExampleItems.java
@@ -1,7 +1,11 @@
 package com.example.examplemod.registry;
 
 import com.example.examplemod.ExampleMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -9,5 +13,6 @@ public class ExampleItems
 {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ExampleMod.MODID);
 
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", ExampleBlocks.EXAMPLE_BLOCK);
+    public static ResourceKey<Item> EXAMPLE_BLOCK_ITEM_RESOURCE_KEY = ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(ExampleMod.MODID, "example_block"));
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.register(EXAMPLE_BLOCK_ITEM_RESOURCE_KEY.location().getPath(), () -> new BlockItem(ExampleBlocks.EXAMPLE_BLOCK.get(), new Item.Properties().setId(EXAMPLE_BLOCK_ITEM_RESOURCE_KEY)));
 }
