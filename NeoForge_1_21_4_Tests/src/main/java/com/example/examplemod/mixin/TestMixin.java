07744a64d810aada66d5ae965ba7ae486ee039fb
Nicolas Hochart <nicolas@freddom.fr>
2025-02-09 02:15:24 +0100

    feat(NeoForge_1_21_4_Tests) : Edited getEffectiveGravity mixin

--- NeoForge_1_21_4_Tests/src/main/java/com/example/examplemod/mixin/TestMixin.java
+++ NeoForge_1_21_4_Tests/src/main/java/com/example/examplemod/mixin/TestMixin.java
@@ -2,6 +2,7 @@ package com.example.examplemod.mixin;
 
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,12 +12,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class TestMixin
 {
     @Inject(method = "getEffectiveGravity", at = @At("RETURN"), cancellable = true)
-    private void onBlockBreaking(CallbackInfoReturnable<Double> callbackInfo)
+    private void onGetEffectiveGravity(CallbackInfoReturnable<Double> callbackInfo)
     {
+        double baseGravity = callbackInfo.getReturnValue();
         LivingEntity entity = (LivingEntity) (Object) this;
-        if (entity.hasEffect(MobEffects.REGENERATION))
+        if (entity.hasEffect(MobEffects.REGENERATION) && !entity.isCrouching())
         {
-            callbackInfo.setReturnValue(-0.1D);
+            Vec3 deltaMovement = entity.getDeltaMovement();
+            if (deltaMovement.y < -0.2D)
+            {
+                double yDeficit = 1.0D / 4.0D * (deltaMovement.y + 0.2D);
+                callbackInfo.setReturnValue(yDeficit);
+                return;
+            }
+            callbackInfo.setReturnValue(baseGravity - 0.05D);
         }
     }
 }
