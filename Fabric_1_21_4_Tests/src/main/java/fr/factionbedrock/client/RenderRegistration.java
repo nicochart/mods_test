6b477896ac48977649286ed5e0912c3634e9b094
Nicolas Hochart <nicolas@freddom.fr>
2025-03-16 15:50:03 +0100

    feat(Fabric_1_21_4_Tests) : Shifting Baked Model

--- Fabric_1_21_4_Tests/src/main/java/fr/factionbedrock/client/RenderRegistration.java
+++ Fabric_1_21_4_Tests/src/main/java/fr/factionbedrock/client/RenderRegistration.java
@@ -1,12 +1,30 @@
 package fr.factionbedrock.client;
 
+import fr.factionbedrock.client.bakedmodels.ShiftingBlockBakedModel;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.model.*;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.Identifier;
 
 public class RenderRegistration
 {
     public static void registerShiftingBakedModels()
+    {
+        ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((original, context) ->
+        {
+            Baker baker = context.baker();
+            if (context.id().toString().equals("test_mod:block/example_block"))
+            {
+                BakedModel glowstoneBakedModel = baker.bake(Identifier.ofVanilla("block/glowstone"), ModelRotation.X0_Y0);
+                BakedModel editedModel = new ShiftingBlockBakedModel(original, glowstoneBakedModel, (forceShifted) -> MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().player.hasStatusEffect(StatusEffects.NIGHT_VISION) || forceShifted);
+                return editedModel;
+            }
+            else {return original;}
+        }));
+    }
+
+    public static void makeEveryBlockRenderLikeStone()
     {
         ModelLoadingPlugin.register(plugin -> plugin.modifyModelAfterBake().register((original, context) ->
         {
