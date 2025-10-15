656f6eb877998aeef24af65cb4aff20f0fa626c5
Nicolas Hochart <nicolas@freddom.fr>
2025-10-16 00:20:26 +0200

    feat(Fabric_1_21_4_Tests) : Placing blocks in structure block field

--- Fabric_1_21_4_Tests/src/main/java/fr/factionbedrock/mixin/PlayerTickMixin.java
+++ Fabric_1_21_4_Tests/src/main/java/fr/factionbedrock/mixin/PlayerTickMixin.java
@@ -4,6 +4,9 @@ import fr.factionbedrock.FabricTest;
 import fr.factionbedrock.registry.TestTags;
 import fr.factionbedrock.registry.TestTrackedData;
 import fr.factionbedrock.util.TestHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.StructureBlockBlockEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -11,11 +14,14 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Mixin(ServerPlayerEntity.class)
@@ -26,10 +32,15 @@ public class PlayerTickMixin
     private static final Identifier CLICK_JUMP_MODIFIER = FabricTest.id("click_jump_modifier");
     private static final Identifier CLICK_STEP_HEIGHT_MODIFIER = FabricTest.id("click_step_height_modifier");
 
+    private static final List<StructureBlockBlockEntity> REUSABLE_NEARBY_STRUCTURE_BLOCK_ENTITIES_LIST = new ArrayList<>();
+    private static final BlockPos.Mutable MUTABLE_POS = new BlockPos.Mutable();
+
     @Inject(at = @At("RETURN"), method = "tick")
     private void onTick(CallbackInfo info)
     {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+        BlockPos playerPos = player.getBlockPos();
+        World world = player.getWorld();
         updatePlayerAttributes(player);
 
         int lives = player.getDataTracker().get(TestTrackedData.LIVES);
@@ -62,6 +73,39 @@ public class PlayerTickMixin
                 }
                 player.getDataTracker().set(TestTrackedData.LIVES, Math.min(3, lives + livePlayerCanRegain));
                 player.getDataTracker().set(TestTrackedData.LIVE_REGAIN_TIME_MARKER, currentTime);
+            }
+        }
+
+        //structure block detection every 2 seconds
+        if (player.age % 40 == 0)
+        {
+            REUSABLE_NEARBY_STRUCTURE_BLOCK_ENTITIES_LIST.clear();
+            TestHelper.fillNearbyStructureBlockEntitiesList(REUSABLE_NEARBY_STRUCTURE_BLOCK_ENTITIES_LIST, player.getServerWorld(), playerPos, 64);
+        }
+        //block transformation around player if block is air in a structure block bounding box
+        if (!REUSABLE_NEARBY_STRUCTURE_BLOCK_ENTITIES_LIST.isEmpty())
+        {
+            int dx,dy,dz;
+            int radius = 5;
+            for (dx = -radius; dx <= radius; dx++)
+            {
+                for (dy = -radius; dy <= radius; dy++)
+                {
+                    for (dz = -radius; dz <= radius; dz++)
+                    {
+                        if (dx * dx + dy * dy + dz * dz > radius * radius) continue;
+
+                        MUTABLE_POS.set(playerPos.getX() + dx, playerPos.getY() + dy, playerPos.getZ() + dz);
+
+                        //ignore current iteration if block is not air
+                        if (!world.isAir(MUTABLE_POS)) continue;
+
+                        if (TestHelper.isPosInsideStructureBlockZone(MUTABLE_POS, REUSABLE_NEARBY_STRUCTURE_BLOCK_ENTITIES_LIST))
+                        {
+                            world.setBlockState(MUTABLE_POS, Blocks.TINTED_GLASS.getDefaultState(), Block.NOTIFY_ALL);
+                        }
+                    }
+                }
             }
         }
     }
--- Fabric_1_21_4_Tests/src/main/java/fr/factionbedrock/util/TestHelper.java
+++ Fabric_1_21_4_Tests/src/main/java/fr/factionbedrock/util/TestHelper.java
@@ -3,6 +3,9 @@ package fr.factionbedrock.util;
 import fr.factionbedrock.FabricTest;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.StructureBlockBlockEntity;
+import net.minecraft.block.enums.StructureBlockMode;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.resource.ResourcePackProfile;
@@ -13,6 +16,10 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -87,5 +94,61 @@ public class TestHelper
         loadedPacks = ((numberOfLoadedPacks == 0) ? "0 loaded packs" : numberOfLoadedPacks + " loaded packs : ") + loadedPacks;
         if (loadedPacks.endsWith(" | ")) {loadedPacks = loadedPacks.substring(0, loadedPacks.length() - 3);}
         player.sendMessage(Text.literal(loadedPacks), false);
+    }
+
+    public static void fillNearbyStructureBlockEntitiesList(List<StructureBlockBlockEntity> listToFill, ServerWorld world, BlockPos center, int radius)
+    {
+        int chunkRadius = (radius >> 4) + 1;
+
+        int centerChunkX = center.getX() >> 4;
+        int centerChunkZ = center.getZ() >> 4;
+
+        for (int cx = centerChunkX - chunkRadius; cx <= centerChunkX + chunkRadius; cx++)
+        {
+            for (int cz = centerChunkZ - chunkRadius; cz <= centerChunkZ + chunkRadius; cz++)
+            {
+                Chunk chunk = world.getChunk(cx, cz, ChunkStatus.FULL, false);
+                if (chunk == null) continue;
+
+                for (BlockPos pos : chunk.getBlockEntityPositions())
+                {
+                    if (pos.isWithinDistance(center, radius))
+                    {
+                        BlockEntity blockentity = chunk.getBlockEntity(pos);
+                        if (blockentity instanceof StructureBlockBlockEntity structureBlockEntity)
+                        {
+                            listToFill.add(structureBlockEntity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean isPosInsideStructureBlockZone(BlockPos pos, List<StructureBlockBlockEntity> structureBlockEntities)
+    {
+        for (StructureBlockBlockEntity structureBlockEntity : structureBlockEntities)
+        {
+            BlockPos offset = structureBlockEntity.getOffset();
+            BlockPos origin = structureBlockEntity.getPos().add(offset);
+            Vec3i size = structureBlockEntity.getSize();
+            StructureBlockMode mode = structureBlockEntity.getMode();
+
+            //ignore current iteration if structure block is not in "save" mode
+            if (mode != StructureBlockMode.SAVE) continue;
+
+            BlockPos min = origin;
+            BlockPos max = origin.add(size.getX() - 1, size.getY() - 1, size.getZ() - 1);
+
+            int minX = Math.min(min.getX(), max.getX());
+            int minY = Math.min(min.getY(), max.getY());
+            int minZ = Math.min(min.getZ(), max.getZ());
+            int maxX = Math.max(min.getX(), max.getX());
+            int maxY = Math.max(min.getY(), max.getY());
+            int maxZ = Math.max(min.getZ(), max.getZ());
+
+            if (pos.getX() >= minX && pos.getX() <= maxX && pos.getY() >= minY && pos.getY() <= maxY && pos.getZ() >= minZ && pos.getZ() <= maxZ) {return true;}
+        }
+        return false;
     }
 }
